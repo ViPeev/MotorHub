5a57bcd495a32af63e0dbedf06c2ed1ef07f0fc7
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-03-05 12:20:45 +0200

    finished edit styles

--- src/Components/Edit/Edit.js
+++ src/Components/Edit/Edit.js
@@ -1,11 +1,33 @@
 import CreateFormPartial from "../Forms/CreateFormPartial";
 import ImageUpload from "../Misc/ImageUploader/ImageUploader";
-import styles from "../Create/Create.module.scss";
+import styles from "./Edit.module.scss";
 import { useState } from "react";
 import data from "../../staticData/formData";
 
+let mockData = {
+  category: "Saloon / Sedan",
+  condition: "Used",
+  cubicCapacity: "2200",
+  color: "Red",
+  doors: "4/5",
+  description: "mnogo dobra",
+  fuelType: "Diesel",
+  images: [],
+  location: "Bulgaria",
+  make: "Renault",
+  model: "Twingo",
+  modification: "16v",
+  mileage: "150000",
+  power: "67",
+  price: "3000",
+  seats: "4",
+  transmission: "Manual",
+  phone: "+359876511111",
+  year: "2005",
+}
+
 export default function Edit() {
-  const [formData, setFormData] = useState(data.createFormData);
+  const [formData, setFormData] = useState(mockData);
 
   const handleChange = (e) => {
     setFormData((data) => {
@@ -24,13 +46,19 @@ export default function Edit() {
   return (
     <main className={styles.main}>
       <section>
-        <form onSubmit={handleSubmit} className={styles["data-form"]}>
-          <CreateFormPartial
-            handleChange={handleChange}
-            data={data}
-            formData={formData}
-          />
-          <ImageUpload formData={formData} setFormData={setFormData} />
+        <h2>Edit article</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <CreateFormPartial
+              handleChange={handleChange}
+              data={data}
+              formData={formData}
+            />
+          </div>
+          <div>
+            <ImageUpload formData={formData} setFormData={setFormData} />
+          </div>
+          <button>Edit</button>
         </form>
       </section>
     </main>
