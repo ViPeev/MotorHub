bc07bb444495816a7acf108b972fe831bee7f097
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-03-21 22:57:38 +0200

    fixed logout button bug

--- src/Components/Misc/Error/ErrorBox.js
+++ src/Components/Misc/Error/ErrorBox.js
@@ -2,8 +2,13 @@ import { createPortal } from "react-dom";
 import styles from "./ErrorBox.module.scss";
 
 export default function ErrorBox({ text }) {
-  return createPortal(
-    <span className={`${styles["error-box"]} fade-in`}>{text}</span>,
-    document.body
-  );
+  const content = text.split("\n");
+
+  const errors = content.map((current, index) => (
+    <span key={index} className={`${styles["error-box"]} fade-in`}>
+      {current}
+    </span>
+  ));
+
+  return createPortal(errors, document.body);
 }
