954abbfe0c28297612b31994416e25d31800865b
Vladislav Peev <vladislavpeev9412@gmail.com>
2023-03-21 23:55:44 +0200

    added global error boundary

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import PublicRoute from "./Components/Guards/PublicOnly";
 import OwnerRoute from "./Components/Guards/OwnerOnly";
 import Edit from "./Components/Edit/Edit";
 import CarDetails from "./Components/Details/CarDetails";
+import ErrorBoundary from "./Components/ErrorHandlers/ErrorBoundary";
+import Fallback from "./Components/ErrorHandlers/Fallback";
 import { Skeleton } from "./Components/Misc/Loaders/Loaders";
 import { SearchProvider } from "./contexts/SearchContext";
 import { ProfileProvider } from "./contexts/ProfileContext";
@@ -20,44 +22,46 @@ const SellerProfile = React.lazy(() => import("./Components/Profile/SellerProfil
 const LoginPage = React.lazy(() => import("./Components/Auth/LoginPage"));
 const RegisterPage = React.lazy(() => import("./Components/Auth/RegisterPage"));
 const Logout = React.lazy(() => import("./Components/Auth/Logout"));
-const NotFound = React.lazy(() => import("./Components/404/404"));
+const NotFound = React.lazy(() => import("./Components/ErrorHandlers/404"));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <SearchProvider>
-          <ProfileProvider>
+          <SearchProvider>
+            <ProfileProvider>
 
-          <Header />
-          <Routes>
-            <Route path="/" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Home /></Suspense>} />
-            <Route path="/cars" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Catalogue /></Suspense>} />            
-            <Route path="/sellers/:id" element={<Suspense fallback={<Skeleton height={"suspense"} />}><SellerProfile /></Suspense>} />
-            <Route path="/news" element={<main>news</main>} />
-            <Route path="/forum" element={<main>forum</main>} />
-            <Route path="/details/:id" element={<CarDetails />} />
+              <Header />
+              <ErrorBoundary fallback={<Fallback />}>
+              <Routes>
+                <Route path="/" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Home /></Suspense>} />
+                <Route path="/cars" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Catalogue /></Suspense>} />            
+                <Route path="/sellers/:id" element={<Suspense fallback={<Skeleton height={"suspense"} />}><SellerProfile /></Suspense>} />
+                <Route path="/news" element={<main>news</main>} />
+                <Route path="/forum" element={<main>forum</main>} />
+                <Route path="/details/:id" element={<CarDetails />} />
 
-            <Route element={<PrivateRoute />}>
-              <Route path="/sell" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Create /></Suspense>} />
-              <Route path="/logout" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Logout /></Suspense>} />
-              <Route path="/profile" element={<Suspense fallback={<Skeleton height={"suspense"} />}><UserProfile /></Suspense>} />
+                <Route element={<PrivateRoute />}>
+                  <Route path="/sell" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Create /></Suspense>} />
+                  <Route path="/logout" element={<Suspense fallback={<Skeleton height={"suspense"} />}><Logout /></Suspense>} />
+                  <Route path="/profile" element={<Suspense fallback={<Skeleton height={"suspense"} />}><UserProfile /></Suspense>} />
 
-              <Route element={<OwnerRoute />}>
-                <Route path="/edit/:id" element={<Edit />} />
-              </Route>
-            </Route>
+                  <Route element={<OwnerRoute />}>
+                    <Route path="/edit/:id" element={<Edit />} />
+                  </Route>
+                </Route>
 
-            <Route element={<PublicRoute />}>
-              <Route path="/register" element={<Suspense fallback={<Skeleton height={"suspense"} />}><RegisterPage /></Suspense>} />
-              <Route path="/login" element={<Suspense fallback={<Skeleton height={"suspense"} />}><LoginPage /></Suspense>} />
-            </Route>
-            <Route path="*" element={<Suspense fallback={<Skeleton height={"suspense"}></Skeleton>}><NotFound /></Suspense>} />
-          </Routes>
-          <Footer />
+                <Route element={<PublicRoute />}>
+                  <Route path="/register" element={<Suspense fallback={<Skeleton height={"suspense"} />}><RegisterPage /></Suspense>} />
+                  <Route path="/login" element={<Suspense fallback={<Skeleton height={"suspense"} />}><LoginPage /></Suspense>} />
+                </Route>
+                <Route path="*" element={<Suspense fallback={<Skeleton height={"suspense"}></Skeleton>}><NotFound /></Suspense>} />
+              </Routes>
+              </ErrorBoundary>
+              <Footer />
 
-          </ProfileProvider>
-        </SearchProvider>
+            </ProfileProvider>
+          </SearchProvider>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/ErrorHandlers/Fallback.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./404.module.scss";
+import image from "../../assets/images/car-fix.png";
+
+export default function Fallback() {
+  return (
+    <main className={styles.main}>
+      <div className={styles["message-box"]}>
+        <p>Something went wrong !</p>
+      </div>
+      <div className={styles["fb-image-box"]}>
+        <img src={image} alt="Something went wrong" />
+      </div>
+    </main>
+  );
+}
